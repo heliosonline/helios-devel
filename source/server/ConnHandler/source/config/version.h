38744fbe4db507fecc29f2621d5c1de7007f658e
Pernicius <pernicius@web.de>
2022-10-10 05:06:43 +0200

    i must rethink the use of version.h...

--- source/server/ConnHandler/source/config/version.h
+++ source/server/ConnHandler/source/config/version.h
@@ -17,4 +17,4 @@
 #include "Common/version.h"
 
 #define VERSION_PATCH 0
-#define VERSION_BUILD 105 // will automatically incremented by build_inc
+#define VERSION_BUILD 114 // will automatically incremented by build_inc
